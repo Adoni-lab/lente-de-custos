443f6739703e794cc6cf091430de321fbd13a227
Adoni-lab <adonimenezes@gmail.com>
2025-10-15 14:39:21 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Configuração compatível com builds na Vercel
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  base: "./", // ESSENCIAL para funcionar no Vercel
+  base: "./", // garante caminhos relativos no build
+  server: { port: 5173 }
 });
